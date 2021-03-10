6ca8b948884a02f26866352ba7026442abc943c2
karasawa-toshiya <n89.karasawa.toshiya@gmail.com>
2021-03-11 00:38:24 +0900

    単体テストを実装修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :encrypted_password, :password_confirmation, length: { minimum: 6 },
-                                                         format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/, message: 'Including half-width alphanumeric characters' }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is Full-width characters' }
-  validates :family_name_kana, presence: true,
-                               format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than alphanumerics' }
-  validates :first_name_kana, presence: true,
-                              format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than alphanumerics' }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is Full-width characters' }
+    validates :family_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than alphanumerics' }
+    validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than alphanumerics' }
+    validates :birthday
+  end
+
+  validates :password, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/, message: 'Including half-width alphanumeric characters' }
+
 end
