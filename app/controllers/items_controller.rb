860dc297188ed1c6c0fba0badec9bd555cd7d8ae
karasawa-toshiya <n89.karasawa.toshiya@gmail.com>
2021-03-14 02:48:28 +0900

    商品出品情報を保存するためのモデルとテーブル作成＆ActiveStorageを導入＆ActiveHashを導入＆バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @user = User.find(params[:user_id])
+    @items = @user.items.includes(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @item = @room.items.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @items = @user.items.includes(:user)
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_charges_id, :shipping_area_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
-  belong_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :shipping_charges_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :delivery_days_id, numericality: { other_than: 1 }
+    validates :price
+    validates :image
+  end
+
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :delivery_days
 end
