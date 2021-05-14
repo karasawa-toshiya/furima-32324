1136bd68d34447036b5f617e9b2ff83de975056b
karasawa-toshiya <n89.karasawa.toshiya@gmail.com>
2021-05-14 21:06:32 +0900

    自動デプロイ機能実装　修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,11 +11,11 @@ set :repo_url, 'git@github.com:karasawa-toshiya/furima-32324.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'このアプリで使用しているrubyのバージョン' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/ご自身のキーペア名.pem'] 
+                                  keys: ['~/.ssh/toshiyatest.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
